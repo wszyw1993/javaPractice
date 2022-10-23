f15b7475b27a27615d53b2f86bd3bff4a45f6bf7
ywz <646694522@qq.com>
2022-10-23 19:38:47 +0800

    the first commit

---
+++ src/com/javaPractice/start/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.javaPractice.start;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ywz
+ *
+ */
+module JavaPractice {
+}
